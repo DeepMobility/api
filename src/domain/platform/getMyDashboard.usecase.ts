7753c72791e0470b09f7385313af0168785922af
forster <groux.forster@gmail.com>
2025-02-20 19:01:33 +0100

    update get dashboard with last user session

--- src/domain/platform/getMyDashboard.usecase.ts
+++ src/domain/platform/getMyDashboard.usecase.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../database/entities/user.entity';
 import { Video } from '../../database/entities/video.entity';
+import { Session } from 'src/database/entities/session.entity';
 
 @Injectable()
 export class GetMyDashboardUsecase {
@@ -16,27 +17,52 @@ export class GetMyDashboardUsecase {
   async get(
     userId: string,
   ): Promise<any> {
-    const user = await this.usersRepository.findOneBy({ id: userId });
+    const user = await this.usersRepository.findOne({
+      relations: { sessions: { video: true} },
+      where: { id: userId },
+    });
 
     const videos = await this.videosRepository.find();
 
+    const course = user.jobType === 'remote' ? 'sitting' : user.jobType;
+
+    const courseVideos = videos
+      .filter((video) => video.course === course)
+      .sort((v1, v2) => v1.coursePosition - v2.coursePosition)
+
+    const orderedUserSessions = user.sessions.sort((s1, s2) => s2.createdAt.getTime() - s1.createdAt.getTime());
+
+    const lastUserSession = orderedUserSessions.length ? orderedUserSessions[orderedUserSessions.length - 1] : null;
+
+    const dailySessionDone = lastUserSession ? lastUserSession.createdAt.toDateString() === (new Date()).toDateString() : false
+
+    const dailyVideo = getDailyVideo(lastUserSession, courseVideos, dailySessionDone);
+
     return {
       name: user.firstName,
-      jobType: user.jobType,
-      videos: videos.map((video) => {
-        return {
-          id : video.id,
-          name: video.name,
-          url: video.url,
-          thumbnailUrl: video.thumbnailUrl,
-          description: video.description,
-          duration: video.duration,
-          course: video.course,
-          coursePosition: video.coursePosition,
-          bodyParts: video.bodyParts,
-          exerciseTypes: video.exerciseTypes,
-        }
-      })
+      dailyVideo,
+      dailySessionDone,
+      course,
+      courseVideos,
+      videos,
     };
   }
+}
+
+function getDailyVideo(lastUserSession: Session, courseVideos: Video[], dailySessionDone: boolean) {
+  if (!lastUserSession) {
+    return courseVideos[0]
+  }
+
+  if (dailySessionDone) {
+    return lastUserSession.video
+  }
+
+  const lastSessionVideoIndex = courseVideos.findIndex((video) => video.id === lastUserSession.video.id)
+
+  if (lastSessionVideoIndex === courseVideos.length - 1) {
+    return courseVideos[0]
+  }
+
+  return courseVideos[lastSessionVideoIndex + 1]
 }
