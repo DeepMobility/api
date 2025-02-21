914f4e3c36e9af4fbdb22c404eef1765b4a00166
forster <groux.forster@gmail.com>
2025-02-21 16:09:39 +0100

    add scalingo config

--- src/domain/admin/addAccount.usecase.ts
+++ src/domain/admin/addAccount.usecase.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Account } from '../../database/entities/account.entity';
+import nodeFetch from 'node-fetch'
 
 @Injectable()
 export class AddAccountUsecase {
@@ -10,7 +11,36 @@ export class AddAccountUsecase {
     private accountsRepository: Repository<Account>,
   ) {}
 
-  add(name: string, slug: string, host: string, logoUrl: string): Promise<any> {
+  async add(name: string, slug: string, host: string, logoUrl: string): Promise<any> {    
+    let requestOptions: RequestInit = {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    };
+
+    const authResponse = await nodeFetch(`https://:${process.env.SCALINGO_API_TOKEN}@auth.scalingo.com/v1/tokens/exchange`, requestOptions);
+
+    const authBody = await authResponse.json();
+    const token = authBody.token;
+
+    requestOptions.headers["Authorization"] = `Bearer ${token}`;
+
+    requestOptions.body = JSON.stringify({
+      domain: { name: host }
+    });
+
+    await fetch(`https://api.osc-fr1.scalingo.com/v1/apps/${process.env.SCALINGO_APP_NAME}/domains`, requestOptions);
+
+    requestOptions.body = JSON.stringify({ scope: null });
+
+    await Promise.all([
+      fetch(`https://api.osc-fr1.scalingo.com/v1/apps/${process.env.SCALINGO_API_NAME}/restart`, requestOptions),
+      fetch(`https://api.osc-fr1.scalingo.com/v1/apps/${process.env.SCALINGO_APP_NAME}/restart`, requestOptions)
+    ]);
+
     return this.accountsRepository.save({ name, slug, host, logoUrl });
   }
 }
